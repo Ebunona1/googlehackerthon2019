b8fa604abfa52ec633d7a73e8f27a8a582f2ac4e
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2019-10-26 19:42:01 +0100

    feat: navbar

--- pitchify/src/index.js
+++ pitchify/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
+    return (
+
+        <div>
+            <NavBar></NavBar>
+            <Button variant="contained" color="primary">
+                Hello World
     </Button>
-  );
+        </div>
+
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector("#app"));
