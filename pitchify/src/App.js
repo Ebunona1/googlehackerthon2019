d7293383a750e47f340248b2d9578ba46a743d66
sophie-norman <s.norman2@newcastle.ac.uk>
2019-10-26 19:23:32 +0100

    playing basic sound

--- pitchify/src/App.js
+++ pitchify/src/App.js
@@ -4,8 +4,41 @@ import './App.css';
 
 function App() {
 
+  var sourceNode;
+  var analyser;
+  var playing = false;
+  var audioContext;
+
+  var isAudioContextSupported = function () {
+    // This feature is still prefixed in Safari
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    if(window.AudioContext){
+        return true;
+    }
+    else {
+        return false;
+    }
+  };
+
+  if(isAudioContextSupported()) {
+      audioContext = new window.AudioContext();
+      sourceNode = audioContext.createOscillator();
+      analyser = audioContext.createAnalyser();
+  }
+
   var x = function () {
-    console.log("hello")
+    analyser.fftSize = 2048;
+    sourceNode.connect( analyser );
+    analyser.connect( audioContext.destination );
+    
+    start();
+  }
+
+  var start = function () {
+    sourceNode.start(0);
+    sourceNode.stop(1);
+    playing = true;
+    console.log("playing");
   }
 
   return (
