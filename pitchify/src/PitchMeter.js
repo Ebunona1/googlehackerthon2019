3b6dda496507e05f12f860697e9ee7c74f2a0404
sophie-norman <s.norman2@newcastle.ac.uk>
2019-10-27 01:54:25 +0100

    separated files

---
+++ pitchify/src/PitchMeter.js
@@ -0,0 +1,113 @@
+import AudioContextApi from './AudioContextApi';
+import freqTable from './notes';
+var PitchMeter = (() => {
+    const FREQ_MATCHING_THRESHOLD = 5;
+    var detectPitch = function (analyserAudioNode) {
+      var buffer = new Uint8Array(analyserAudioNode.fftSize);
+      // See initializations in the AudioContent and AnalyserNode sections of the demo.
+      analyserAudioNode.getByteTimeDomainData(buffer);
+      var fundalmentalFreq = findFundamentalFreq(buffer, AudioContextApi.getAudioContext().sampleRate);
+      return fundalmentalFreq;
+    };
+  
+    var findFundamentalFreq = function (buffer, sampleRate) {
+          // We use Autocorrelation to find the fundamental frequency.
+  
+          // In order to correlate the signal with itself (hence the name of the algorithm), we will check two points 'k' frames away.
+          // The autocorrelation index will be the average of these products. At the same time, we normalize the values.
+          // Source: http://www.phy.mty.edu/~suits/autocorrelation.html
+          // Assuming the sample rate is 48000Hz, a 'k' equal to 1000 would correspond to a 48Hz signal (48000/1000 = 48),
+          // while a 'k' equal to 8 would correspond to a 6000Hz one, which is enough to cover most (if not all)
+          // the notes we have in the notes.json file.
+          var n = 1024;
+          var bestK = -1;
+          var bestR = 0;
+          for (var k = 8; k <= 1000; k++) {
+              var sum = 0;
+  
+              for (var i = 0; i < n; i++) {
+                  sum += ((buffer[i] - 128) / 128) * ((buffer[i + k] - 128) / 128);
+              }
+  
+              var r = sum / (n + k);
+  
+              if (r > bestR) {
+                  bestR = r;
+                  bestK = k;
+              }
+  
+              if (r > 0.9) {
+                  // Let's assume that this is good enough and stop right here
+                  break;
+              }
+          }
+  
+          if (bestR > 0.0025) {
+              // The period (in frames) of the fundamental frequency is 'bestK'. Getting the frequency from there is trivial.
+              var fundamentalFreq = sampleRate / bestK;
+              return fundamentalFreq;
+          }
+          else {
+              // We haven't found a good correlation
+              return -1;
+          }
+      };
+    var withinPercent = (target, actual, threshold) => {
+      var diff = target - actual;
+      var percent = (diff/target) * 100;
+      console.log("percent",actual);
+      return Math.abs(percent) <= threshold;
+    }
+    var matchPitch = async (targetFreq, maxSamples,onPitchMatched, onTimeout) => {
+      var notesTable = freqTable[440];
+      let stream = await navigator.mediaDevices.getUserMedia({audio: true}).then((micStream) => {
+        var analyserAudioNode = AudioContextApi.getAudioContext().createAnalyser();
+        analyserAudioNode.fftSize = 2048;
+      
+        var sourceAudioNode = AudioContextApi.getAudioContext().createMediaStreamSource(micStream);
+        
+        sourceAudioNode.connect(analyserAudioNode); // See initialization in the AnalyserNode section of the demo.
+        /* This is our pitch detection algorithm.
+          You can find its implementation in the Autocorrelation section of this demo. */
+        
+          for (var samples = 0; samples < maxSamples; samples ++) {
+            var freq = detectPitch(analyserAudioNode);
+            // break when freq matches expected
+            if (withinPercent(targetFreq, freq, FREQ_MATCHING_THRESHOLD)) {
+              onPitchMatched();
+              console.log(findClosestNote(freq, notesTable));
+            }
+            
+          }
+          onTimeout();
+      });
+  
+    };
+    var findClosestNote = (freq, notes) => {
+      if (freq < 0)
+        return;
+      var low = -1, high = notes.length;
+      while (high - low > 1){
+        var pivot = Math.round((low + high) / 2);
+        console.log(notes[pivot].frequency);
+        if (notes[pivot].frequency <= freq) {
+          low = pivot;
+        } else {
+          high = pivot;
+        }
+      }
+  
+      if(Math.abs(notes[high].frequency - freq) <= Math.abs(notes[low].frequency - freq)) {
+        return notes[high];
+      }
+  
+      return notes[low];
+    }
+    return {
+      // maxPitch(targetFreq, maxSamples,onPitchMatched, onTimeout)
+      matchPitch: matchPitch,
+      findNotes: findClosestNote
+    };
+  })();
+
+  export default PitchMeter;
--- pitchify/src/index.js
+++ pitchify/src/index.js
@@ -1,142 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
-
-var AudioContextApi = (() => {
-  var audioContext;
-
-  var createAudioContext = function () {
-    var LocalAudioContext = window.AudioContext || window.webkitAudioContext;
-    return new LocalAudioContext();
-  }
-  var getAudioContext = () => {
-    if (!audioContext) {
-      audioContext = createAudioContext();
-    }
-    return audioContext;
-  }
-  return {
-    getAudioContext: getAudioContext
-  };
-})();
-
-var PitchMeter = (() => {
-  const FREQ_MATCHING_THRESHOLD = 20;
-  var detectPitch = function (analyserAudioNode) {
-    var buffer = new Uint8Array(analyserAudioNode.fftSize);
-    // See initializations in the AudioContent and AnalyserNode sections of the demo.
-    analyserAudioNode.getByteTimeDomainData(buffer);
-    var fundalmentalFreq = findFundamentalFreq(buffer, AudioContextApi.getAudioContext().sampleRate);
-    return fundalmentalFreq;
-  };
-
-  var findFundamentalFreq = function (buffer, sampleRate) {
-		// We use Autocorrelation to find the fundamental frequency.
-
-		// In order to correlate the signal with itself (hence the name of the algorithm), we will check two points 'k' frames away.
-		// The autocorrelation index will be the average of these products. At the same time, we normalize the values.
-		// Source: http://www.phy.mty.edu/~suits/autocorrelation.html
-		// Assuming the sample rate is 48000Hz, a 'k' equal to 1000 would correspond to a 48Hz signal (48000/1000 = 48),
-		// while a 'k' equal to 8 would correspond to a 6000Hz one, which is enough to cover most (if not all)
-		// the notes we have in the notes.json file.
-		var n = 1024;
-		var bestK = -1;
-		var bestR = 0;
-		for (var k = 8; k <= 1000; k++) {
-			var sum = 0;
-
-			for (var i = 0; i < n; i++) {
-				sum += ((buffer[i] - 128) / 128) * ((buffer[i + k] - 128) / 128);
-			}
-
-			var r = sum / (n + k);
-
-			if (r > bestR) {
-				bestR = r;
-				bestK = k;
-			}
-
-			if (r > 0.9) {
-				// Let's assume that this is good enough and stop right here
-				break;
-			}
-		}
-
-		if (bestR > 0.0025) {
-			// The period (in frames) of the fundamental frequency is 'bestK'. Getting the frequency from there is trivial.
-			var fundamentalFreq = sampleRate / bestK;
-			return fundamentalFreq;
-		}
-		else {
-			// We haven't found a good correlation
-			return -1;
-		}
-	};
-  var withinPercent = (target, actual, threshold) => {
-    var diff = target - actual;
-    var percent = (diff/target) * 100;
-    console.log("percent",actual);
-    return Math.abs(percent) <= threshold;
-  }
-  var matchPitch = async (targetFreq, maxSamples,onPitchMatched, onTimeout) => {
-    let stream = await navigator.mediaDevices.getUserMedia({audio: true}).then((micStream) => {
-      var analyserAudioNode = AudioContextApi.getAudioContext().createAnalyser();
-      analyserAudioNode.fftSize = 2048;
-    
-      var sourceAudioNode = AudioContextApi.getAudioContext().createMediaStreamSource(micStream);
-      
-      sourceAudioNode.connect(analyserAudioNode); // See initialization in the AnalyserNode section of the demo.
-      /* This is our pitch detection algorithm.
-        You can find its implementation in the Autocorrelation section of this demo. */
-      
-        for (var samples = 0; samples < maxSamples; samples ++) {
-          var freq = detectPitch(analyserAudioNode);
-          // break when freq matches expected
-          if (withinPercent(targetFreq, freq, FREQ_MATCHING_THRESHOLD)) {
-            onPitchMatched();
-          }
-          
-        }
-        onTimeout();
-    });
-
-  };
-  return {
-    // maxPitch(targetFreq, maxSamples,onPitchMatched, onTimeout)
-    matchPitch: matchPitch,
-  };
-})();
-
-var AudioPlayer = (() => {
-  var frequency;
-  var sourceNode;
-  var playNote = () => {
-    var audioAnalyser = AudioContextApi.getAudioContext().createAnalyser();
-    audioAnalyser.fftSize = 2048;
-
-    sourceNode = AudioContextApi.getAudioContext().createOscillator();
-    audioAnalyser.connect( AudioContextApi.getAudioContext().destination );
-    sourceNode.connect( audioAnalyser );
-    sourceNode.frequency.setValueAtTime(frequency,0);
-
-    sourceNode.start(0);
-  };
-  var setFrequency = (selectedFreq) => {
-    frequency = selectedFreq;
-  }
-  var stopPlaying = () => {
-    sourceNode.stop()
-  }
-  var getFrequency = () => {
-    return sourceNode.frequency.value;
-  }
-  return {
-    playNote: playNote,
-    setFrequency: setFrequency,
-    stopPlaying: stopPlaying,
-    getFrequency: getFrequency
-  };
-})();
+import AudioPlayer from './AudioPlayer';
+import PitchMeter from './PitchMeter';
 
 function App() {
 
@@ -158,8 +24,6 @@ function App() {
     }
   }
   
-
-
   var record = async function () {
     PitchMeter.matchPitch(200., 1000, () => onMatched(), () => {console.log("timeout"); return;});
   }
